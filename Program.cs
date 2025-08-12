6cafc11cf38976e0819d3ff89a16c025b049b905
zenin5479 <zenin5479@gmail.com>
2025-08-12 19:04:10 +0300

    Update 0.2 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -2,7 +2,21 @@
 
 namespace Homework_6._1
 {
-    internal class Program
+   struct Student
+   {
+      char group;
+      char surname;
+      char name;
+      char dadsname;
+      int year;
+      char gender;
+      int physics;
+      int math;
+      int inf;
+      float grant;
+   };
+
+   internal class Program
     {
         static void Main(string[] args)
         {
