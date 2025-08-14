694579b814483472b2befbd57a9f5171a554cff8
zenin5479 <zenin5479@gmail.com>
2025-08-14 10:06:47 +0300

    Update 5.4 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -50,57 +50,8 @@ namespace Homework_6._1
          // Чтение файла
          string fileText = File.ReadAllText(path, Encoding.Unicode);
          Console.WriteLine(fileText);
+         
          Console.ReadKey();
-
-         //
-         //Console.OutputEncoding = Encoding.UTF8;
-         //string inputFile = "input.txt";
-         //Toy[] toys = ReadFile(inputFile);
-         //// Выводим исходный массив игрушек
-         //Console.WriteLine("--------Исходный массив--------");
-         //Display(toys);
-         //Console.WriteLine("-------------------------------");
-
-         //
-         // // Путь к файлу
-         //string path = "note.txt";
-         // Путь к файлу
-         //string path = "input.txt";
-         //// Строка для записи
-         //string text = "Самолет";
-
-         //// запись в файл
-         //using (FileStream fstream = new FileStream(path, FileMode.OpenOrCreate))
-         //{
-         //   // преобразуем строку в байты
-         //   byte[] buffer = Encoding.Default.GetBytes(text);
-         //   // запись массива байтов в файл
-         //   fstream.WriteAsync(buffer, 0, buffer.Length);
-         //   Console.WriteLine("Текст записан в файл");
-         //}
-
-         //// чтение из файла
-         //using (FileStream fstream = File.OpenRead(path))
-         //{
-         //   // выделяем массив для считывания данных из файла
-         //   byte[] buffer = new byte[fstream.Length];
-         //   // считываем данные
-         //   fstream.ReadAsync(buffer, 0, buffer.Length);
-         //   // декодируем байты в строку
-         //   string textFromFile = Encoding.Default.GetString(buffer);
-         //   Console.WriteLine($"Текст из файла: {textFromFile}");
-         //}
-
-         //string originalText = "IP-21 Ivanov Ivan Ivanovich 2000 M 4 5 3 5000 " +
-         //                      "IP-21 Petrova Anna Sergeevna 2001 W 5 4 5 6000 " +
-         //                      "IP-22 Smirnov Alexey Viktorovich 1999 M 3 4 4 4000 " +
-         //                      "Fiz-21 Kuznetsova Maria Pavlovna 2000 W 5 5 5 7000 " +
-         //                      "Phys-22 Sidorov Dmitry Andreevich 2001 M 4 3 4 4500 " +
-         //                      "IP-22 Vasilyeva Ekaterina Nikolaevna 1009 W 3 5 4 5500  " +
-         //                      "Fiz-21 Orlov Sergey Vladimirovich 2000 M 4 4 3 3000 IP-21 " +
-         //                      "Lebedeva Svetlana Alexandrovna 2001 W 5 5 5 8000 Fiz-22 " +
-         //                      "Nikolaev Andrey Sergeevich 2007 M 3 2 3 2500 IP-22 " +
-         //                      "Sergeeva Daria Viktorovna 2007 W 2 2 2 5000";
       }
    }
 }
