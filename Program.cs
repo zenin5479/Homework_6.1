55401d52601fe6c6f1af0683475a6e3401a97401
zenin5479 <zenin5479@gmail.com>
2025-08-17 16:35:41 +0300

    Update 11.3 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -109,7 +109,8 @@ namespace Homework_6._1
          Console.WriteLine("Прочитанные данные:");
          foreach (var person in readPeople)
          {
-            Console.WriteLine("{0} {1} {2} {3}", person.Group, person.Surname, person.Name, person.Dadsname);
+            Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}", person.Group, person.Surname, person.Name,
+               person.Dadsname, person.Year, person.Gender, person.Physics, person.Math, person.Inf, person.Grant);
          }
 
          Console.ReadKey();
