d2c7e402d25d64a05f9121a051ad4a4fa3065e5b
zenin5479 <zenin5479@gmail.com>
2025-08-18 22:22:00 +0300

    Update 13.0 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -100,8 +100,7 @@ namespace Homework_6._1
          Console.WriteLine("Прочитанные данные:");
          for (int i = 0; i < readPeople.Length; i++)
          {
-            
-            var person = readPeople[i];
+            Student person = readPeople[i];
             Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}",
                person.Group, person.Surname, person.Name, person.Dadsname, person.Year,
                person.Gender, person.Physics, person.Math, person.Inf, person.Grant);
