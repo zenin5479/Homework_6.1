e9df24335e03be66dc8dab2b504a3b0ba329a36d
zenin5479 <zenin5479@gmail.com>
2025-08-23 09:38:34 +0300

    Update 22.9 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -100,17 +100,7 @@ namespace Homework_6._1
 
          // Запись структур в файл
          WriteStructFile(pathStruct, people);
-        
-         // Вывод прочитанных данных
-         Console.WriteLine("Прочитанные данные:");
-         for (int i = 0; i < readPeople.Length; i++)
-         {
-            Student person = readPeople[i];
-            Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}",
-               person.Group, person.Surname, person.Name, person.Dadsname, person.Year,
-               person.Gender, person.Physics, person.Math, person.Inf, person.Grant);
-         }
-
+         
          Student[] read = MethodsForStruct.ReadStructFile(pathStruct, "spisok.txt");
 
          Console.WriteLine("Прочитанные данные:");
