b98c3b799fb8c0c4eca494717543c4ff933184b1
zenin5479 <zenin5479@gmail.com>
2025-08-24 21:37:48 +0300

    Update 25.1 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -124,12 +124,21 @@ namespace Homework_6._1
          Student[] read = MethodsForStruct.ReadStructFile(pathStruct, "spisok.txt");
          // Вывод прочитанных данных
          Console.WriteLine("Прочитанные данные:");
-         for (int i = 0; i < read.Length; i++)
+        int i = 0;
+         while (i < read.Length)
          {
+            
+            
             Student personTwo = read[i];
             Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}",
                personTwo.Group, personTwo.Surname, personTwo.Name, personTwo.Dadsname, personTwo.Year,
                personTwo.Gender, personTwo.Physics, personTwo.Math, personTwo.Inf, personTwo.Grant);
+            i++;
+         }
+         
+         for (  )
+         {
+            
          }
 
          // Создаем экземпляр структуры
