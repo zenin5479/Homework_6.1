04a105ef99f09ac4faa6491c79700dd24ae8503b
zenin5479 <zenin5479@gmail.com>
2025-08-31 17:01:18 +0300

    Update 36.5 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
 
 // Обработка студенческой ведомости
@@ -38,6 +39,13 @@ namespace Homework_6._1
       public int Inf;
       public double Grant;
    }
+
+   public struct Point
+   {
+      public int x;
+      public int y;
+   }
+
 
 
    internal class Program
@@ -140,10 +148,43 @@ namespace Homework_6._1
          }
 
 
+         // Create a point struct.
+         Point p;
+         p.x = 1;
+         p.y = 1;
+
+         Console.WriteLine("The value of first point is " + p.x + " and " + p.y + ".");
+
+         // Initialize unmanged memory to hold the struct.
+         IntPtr pnt = Marshal.AllocHGlobal(Marshal.SizeOf(p));
+
+         try
+         {
+
+            // Copy the struct to unmanaged memory.
+            Marshal.StructureToPtr(p, pnt, false);
+
+            // Create another point.
+            Point anotherP;
+
+            // Set this Point to the value of the
+            // Point in unmanaged memory.
+            anotherP = (Point)Marshal.PtrToStructure(pnt, typeof(Point));
+
+            Console.WriteLine("The value of new point is " + anotherP.x + " and " + anotherP.y + ".");
+         }
+         finally
+         {
+            // Free the unmanaged memory.
+            Marshal.FreeHGlobal(pnt);
+         }
+
+         // Demonstrate the use of the SizeOf method of the Marshal class.
+         Console.WriteLine("Number of bytes needed by a Point object: {0}", Marshal.SizeOf(typeof(Point)));
+         Console.WriteLine("Number of bytes needed by a Point object: {0}", Marshal.SizeOf(p));
+
          Console.ReadKey();
       }
-
-      
 
 
 
