7671d05f3aef96392c2194d5f49e7e90c6f2317f
zenin5479 <zenin5479@gmail.com>
2025-09-01 22:21:16 +0300

    Update 41.4 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -60,51 +60,51 @@ namespace Homework_6._1
                Group = "IP-21", Surname = "Иванов", Name = "Иван", Dadsname = "Иванович",
                Year = 2000, Gender = 'М', Physics = 4, Math = 5, Inf = 3, Grant = 5000
             },
-            new Student
-            {
-               Group = "IP-21", Surname = "Петрова", Name = "Анна", Dadsname = "Сергеевна",
-               Year = 2001, Gender = 'Ж', Physics = 5, Math = 4, Inf = 5, Grant = 6000
-            },
-            new Student
-            {
-               Group = "IP-22", Surname = "Смирнов", Name = "Алексей", Dadsname = "Викторович",
-               Year = 1999, Gender = 'M', Physics = 3, Math = 4, Inf = 4, Grant = 4000
-            },
-            new Student
-            {
-               Group = "Fiz-21", Surname = "Кузнецова", Name = "Мария", Dadsname = "Павловна",
-               Year = 2000, Gender = 'Ж', Physics = 5, Math = 5, Inf = 5, Grant = 7000
-            },
-            new Student
-            {
-               Group = "Phys-22", Surname = "Сидоров", Name = "Дмитрий", Dadsname = "Андреевич",
-               Year = 2001, Gender = 'M', Physics = 4, Math = 3, Inf = 4, Grant = 4500
-            },
-            new Student
-            {
-               Group = "IP-22", Surname = "Васильева", Name = "Екатерина", Dadsname = "Николаевна",
-               Year = 2009, Gender = 'Ж', Physics = 3, Math = 5, Inf = 4, Grant = 5500
-            },
-            new Student
-            {
-               Group = "Fiz-21", Surname = "Орлов", Name = "Сергей", Dadsname = "Владимирович",
-               Year = 2000, Gender = 'M', Physics = 4, Math = 4, Inf = 3, Grant = 3000
-            },
-            new Student
-            {
-               Group = "IP-21", Surname = "Лебедева", Name = "Светлана", Dadsname = "Александровна",
-               Year = 2001, Gender = 'Ж', Physics = 5, Math = 5, Inf = 5, Grant = 8000
-            },
-            new Student
-            {
-               Group = "Fiz-22", Surname = "Николаев", Name = "Андрей", Dadsname = "Сергеевич",
-               Year = 2007, Gender = 'M', Physics = 3, Math = 2, Inf = 3, Grant = 2500
-            },
-            new Student
-            {
-               Group = "IP-22", Surname = "Сергеева", Name = "Дарья", Dadsname = "Викторовна",
-               Year = 2007, Gender = 'Ж', Physics = 2, Math = 2, Inf = 2, Grant = 5000
-            },
+            //new Student
+            //{
+            //   Group = "IP-21", Surname = "Петрова", Name = "Анна", Dadsname = "Сергеевна",
+            //   Year = 2001, Gender = 'Ж', Physics = 5, Math = 4, Inf = 5, Grant = 6000
+            //},
+            //new Student
+            //{
+            //   Group = "IP-22", Surname = "Смирнов", Name = "Алексей", Dadsname = "Викторович",
+            //   Year = 1999, Gender = 'M', Physics = 3, Math = 4, Inf = 4, Grant = 4000
+            //},
+            //new Student
+            //{
+            //   Group = "Fiz-21", Surname = "Кузнецова", Name = "Мария", Dadsname = "Павловна",
+            //   Year = 2000, Gender = 'Ж', Physics = 5, Math = 5, Inf = 5, Grant = 7000
+            //},
+            //new Student
+            //{
+            //   Group = "Phys-22", Surname = "Сидоров", Name = "Дмитрий", Dadsname = "Андреевич",
+            //   Year = 2001, Gender = 'M', Physics = 4, Math = 3, Inf = 4, Grant = 4500
+            //},
+            //new Student
+            //{
+            //   Group = "IP-22", Surname = "Васильева", Name = "Екатерина", Dadsname = "Николаевна",
+            //   Year = 2009, Gender = 'Ж', Physics = 3, Math = 5, Inf = 4, Grant = 5500
+            //},
+            //new Student
+            //{
+            //   Group = "Fiz-21", Surname = "Орлов", Name = "Сергей", Dadsname = "Владимирович",
+            //   Year = 2000, Gender = 'M', Physics = 4, Math = 4, Inf = 3, Grant = 3000
+            //},
+            //new Student
+            //{
+            //   Group = "IP-21", Surname = "Лебедева", Name = "Светлана", Dadsname = "Александровна",
+            //   Year = 2001, Gender = 'Ж', Physics = 5, Math = 5, Inf = 5, Grant = 8000
+            //},
+            //new Student
+            //{
+            //   Group = "Fiz-22", Surname = "Николаев", Name = "Андрей", Dadsname = "Сергеевич",
+            //   Year = 2007, Gender = 'M', Physics = 3, Math = 2, Inf = 3, Grant = 2500
+            //},
+            //new Student
+            //{
+            //   Group = "IP-22", Surname = "Сергеева", Name = "Дарья", Dadsname = "Викторовна",
+            //   Year = 2007, Gender = 'Ж', Physics = 2, Math = 2, Inf = 2, Grant = 5000
+            //},
          };
 
          // Запись массива структур в текстовый файл
