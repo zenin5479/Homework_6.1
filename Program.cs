782d06971e26bbdbc200be7d583732c2dd5aa946
zenin5479 <zenin5479@gmail.com>
2025-09-02 10:21:40 +0300

    Update 42.2 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -183,6 +183,22 @@ namespace Homework_6._1
          Console.WriteLine($"Общий размер: {size} байт");
       }
 
+      // Метод записи массива структур в бинарный файл
+      static void WriteStructFile2(string path, Student[] people)
+      {
+         StreamWriter writer = new StreamWriter(path, false, Encoding.Unicode);
+         int i = 0;
+         while (i < people.Length)
+         {
+            Student person = people[i];
+            writer.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}",
+               person.Group, person.Surname, person.Name, person.Dadsname, person.Year,
+               person.Gender, person.Physics, person.Math, person.Inf, person.Grant);
+            i++;
+         }
+         writer.Flush(); writer.Close();
+      }
+
       // Метод записи массива структур в текстовый файл
       static void WriteStructFile(string path, Student[] people)
       {
@@ -199,20 +215,6 @@ namespace Homework_6._1
          writer.Close();
       }
 
-      // Метод записи массива структур в бинарный файл
-      static void WriteStructFile2(string path, Student[] people)
-      {
-         StreamWriter writer = new StreamWriter(path, false, Encoding.Unicode);
-         int i = 0;
-         while (i < people.Length)
-         {
-            Student person = people[i];
-            writer.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}",
-               person.Group, person.Surname, person.Name, person.Dadsname, person.Year,
-               person.Gender, person.Physics, person.Math, person.Inf, person.Grant);
-            i++;
-         }
-         writer.Flush(); writer.Close();
-      }
+
    }
 }
