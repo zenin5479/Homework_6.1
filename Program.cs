fa1821f271bbe7cab460d5aa80fee10fac21484d
zenin5479 <zenin5479@gmail.com>
2025-09-05 11:27:57 +0300

    Update 51.2 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -197,14 +197,6 @@ namespace Homework_6._1
       }
 
 
-      //   {
-      //      Student person = students[i];
-      //      writer.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}",
-      //         person.Group, person.Surname, person.Name, person.Dadsname, person.Year,
-      //         person.Gender, person.Physics, person.Math, person.Inf, person.Grant);
-      //      i++;
-      //   }
-
       //   writer.Close();
       //}
    }
