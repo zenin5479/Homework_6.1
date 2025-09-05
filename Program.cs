ae411b55daa317e9ebb3499f644ad1ed3678302f
zenin5479 <zenin5479@gmail.com>
2025-09-05 11:27:19 +0300

    Update 51.1 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -197,12 +197,6 @@ namespace Homework_6._1
       }
 
 
-      // Метод записи массива структур в текстовый файл ?
-      //static void WriteStructFile(string path, Student[] students)
-      //{
-      //   StreamWriter writer = new StreamWriter(path, false, Encoding.UTF8);
-      //   int i = 0;
-      //   while (i < students.Length)
       //   {
       //      Student person = students[i];
       //      writer.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}",
