8c41ea129a742676fc56fdd04ef28839c7c87178
zenin5479 <zenin5479@gmail.com>
2025-09-06 10:47:48 +0300

    Update 52.7 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -173,8 +173,24 @@ namespace Homework_6._1
          Console.ReadKey();
       }
 
-
-
+      static void BubbleSortByAge(Person[] arr)
+      {
+         int n = arr.Length;
+         for (int i = 0; i < n - 1; i++)
+         {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+               // Сравниваем соседние элементы
+               if (arr[j].Age > arr[j + 1].Age)
+               {
+                  // Меняем местами структуры
+                  Person temp = arr[j];
+                  arr[j] = arr[j + 1];
+                  arr[j + 1] = temp;
+               }
+            }
+         }
+      }
 
       static void QuickSort(Student[] arr, int left, int right)
       {
