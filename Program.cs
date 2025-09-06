6478f6e152c0c23925c44b94a0270a3a3af8f4a3
zenin5479 <zenin5479@gmail.com>
2025-09-06 11:12:02 +0300

    Update 53.8 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -154,7 +154,7 @@ namespace Homework_6._1
       }
 
       // Если нужно сортировать по другим критериям:
-      // Измен условие в сортировке(например, для сортировки по имени используйте string.Compare):
+      // Изменя условие в сортировке(например, для сортировки по имени используйте string.Compare):
       // if (string.Compare(arr[j].Name, arr[j + 1].Name) > 0)
       static void BubbleSortByAge(Student[] cadets)
       {
