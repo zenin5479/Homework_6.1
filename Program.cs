740e95590d63f54bb9f01c9912a0395c785a7827
zenin5479 <zenin5479@gmail.com>
2025-09-06 11:05:54 +0300

    Update 53.6 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -139,14 +139,15 @@ namespace Homework_6._1
 
          Console.WriteLine();
          BubbleSortByAge(readCadets);
-
          Console.WriteLine("Отсортированный массив по возрасту:");
-         for (int index = 0; index < readCadets.Length; index++)
+         int index = 0; 
+         while (index < readCadets.Length)
          {
-            Student person = readCadets[index];
+             Student person = readCadets[index];
             Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}",
                person.Group, person.Surname, person.Name, person.Dadsname, person.Year,
                person.Gender, person.Physics, person.Math, person.Inf, person.Grant);
+            index++;
          }
 
          Console.ReadKey();
