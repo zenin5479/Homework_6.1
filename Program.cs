542e93d199320d053837140b2f70ecc7b26ad898
zenin5479 <zenin5479@gmail.com>
2025-09-08 13:08:54 +0300

    Update 59.6 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -225,16 +225,15 @@ namespace Homework_6._1
             i++;
          }
 
-         
-         //int index = 0;
-         //while (index < readCadets.Length)
-         //{
-         //   Student person = readCadets[index];
-         //   Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}",
-         //      person.Group, person.Surname, person.Name, person.Dadsname, person.Year,
-         //      person.Gender, person.Physics, person.Math, person.Inf, person.Grant);
-         //   index++;
-         //}
+         int index = 0;
+         while (index < cadets.Length)
+         {
+            Student person = cadets[index];
+            Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}",
+               person.Group, person.Surname, person.Name, person.Dadsname, person.Year,
+               person.Gender, person.Physics, person.Math, person.Inf, person.Grant);
+            index++;
+         }
       }
    }
 }
