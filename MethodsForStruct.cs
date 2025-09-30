2fd1d71809bc085c730d9c8488813aa24f8f106e
zenin5479 <zenin5479@gmail.com>
2025-09-30 16:12:08 +0300

    Update 71.7 Обновлен метод. Проведен рефакторинг

--- MethodsForStruct.cs
+++ MethodsForStruct.cs
@@ -6,7 +6,6 @@ namespace Homework_6._1
 {
    public class MethodsForStruct
    {
-
       // Метод записи массива структур в текстовый файл
       public static void WriteStructFileTxt(string path, Student[] students)
       {
@@ -405,46 +404,6 @@ namespace Homework_6._1
          writer.WriteLine("{0} {1} {2} {3}",
             worstAverage.Group, worstAverage.Surname, worstAverage.Name, worstAverage.Dadsname);
          writer.Close();
-      }
-
-      // Метод сортировки массива структур по возрасту
-      public static void BubbleSortByAge(Student[] students)
-      {
-         Console.WriteLine("Отсортированный массив структур по возрасту:");
-         // Если нужно сортировать по другим критериям изменяем условие в сортировке:
-         // используем string.Compare
-         // if (string.Compare(arr[j].Name, arr[j + 1].Name) > 0)
-         int n = students.Length;
-         int i = 0;
-         while (i < n - 1)
-         {
-            int j = 0;
-            while (j < n - i - 1)
-            {
-               // Сравниваем соседние элементы
-               if (students[j].Year > students[j + 1].Year)
-               {
-                  // Меняем местами структуры
-                  Student temp = students[j];
-                  students[j] = students[j + 1];
-                  students[j + 1] = temp;
-               }
-
-               j++;
-            }
-
-            i++;
-         }
-
-         int index = 0;
-         while (index < students.Length)
-         {
-            Student person = students[index];
-            Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}",
-               person.Group, person.Surname, person.Name, person.Dadsname, person.Year,
-               person.Gender, person.Physics, person.Math, person.Inf, person.Grant);
-            index++;
-         }
       }
    }
 }
