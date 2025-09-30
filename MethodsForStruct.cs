ee31cec140fe96c2e69317b59ad9398af60d1657
zenin5479 <zenin5479@gmail.com>
2025-09-30 16:10:40 +0300

    Update 71.6 Обновлен метод. Проведен рефакторинг

--- MethodsForStruct.cs
+++ MethodsForStruct.cs
@@ -6,20 +6,6 @@ namespace Homework_6._1
 {
    public class MethodsForStruct
    {
-      // Определяем структуру
-      public struct Student
-      {
-         public string Group;
-         public string Surname;
-         public string Name;
-         public string Dadsname;
-         public int Year;
-         public char Gender;
-         public int Physics;
-         public int Math;
-         public int Inf;
-         public double Grant;
-      }
 
       // Метод записи массива структур в текстовый файл
       public static void WriteStructFileTxt(string path, Student[] students)
--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using LibraryForStruct;
-using System;
+﻿using System;
 using System.IO;
 
 // Обработка студенческой ведомости
@@ -17,6 +16,21 @@ using System.IO;
 
 namespace Homework_6._1
 {
+   // Определяем структуру
+   public struct Student
+   {
+      public string Group;
+      public string Surname;
+      public string Name;
+      public string Dadsname;
+      public int Year;
+      public char Gender;
+      public int Physics;
+      public int Math;
+      public int Inf;
+      public double Grant;
+   }
+
    internal class Program
    {
       static void Main()
@@ -30,69 +44,69 @@ namespace Homework_6._1
          string fileInput = "finish.txt";
 
          // Создание массива структур
-         VariousMethods.Student[] students =
+         Student[] students =
          {
-            new VariousMethods.Student
+            new Student
             {
                Group = "IP-21", Surname = "Иванов", Name = "Иван", Dadsname = "Иванович",
                Year = 2000, Gender = 'М', Physics = 4, Math = 5, Inf = 3, Grant = 5000
             },
-            new VariousMethods.Student
+            new Student
             {
                Group = "IP-21", Surname = "Петрова", Name = "Анна", Dadsname = "Сергеевна",
                Year = 2001, Gender = 'Ж', Physics = 5, Math = 4, Inf = 5, Grant = 6000
             },
-            new VariousMethods.Student
+            new Student
             {
                Group = "IP-22", Surname = "Смирнов", Name = "Алексей", Dadsname = "Викторович",
                Year = 1999, Gender = 'M', Physics = 3, Math = 4, Inf = 4, Grant = 4000
             },
-            new VariousMethods.Student
+            new Student
             {
                Group = "Fiz-21", Surname = "Кузнецова", Name = "Мария", Dadsname = "Павловна",
                Year = 2000, Gender = 'Ж', Physics = 5, Math = 5, Inf = 5, Grant = 7000
             },
-            new VariousMethods.Student
+            new Student
             {
                Group = "Phys-22", Surname = "Сидоров", Name = "Дмитрий", Dadsname = "Андреевич",
                Year = 2001, Gender = 'M', Physics = 4, Math = 3, Inf = 4, Grant = 4500
             },
-            new VariousMethods.Student
+            new Student
             {
                Group = "IP-22", Surname = "Васильева", Name = "Екатерина", Dadsname = "Николаевна",
                Year = 2008, Gender = 'Ж', Physics = 3, Math = 3, Inf = 3, Grant = 3000
             },
-            new VariousMethods.Student
+            new Student
             {
                Group = "Fiz-21", Surname = "Орлов", Name = "Сергей", Dadsname = "Владимирович",
                Year = 2000, Gender = 'M', Physics = 4, Math = 4, Inf = 3, Grant = 3000
             },
-            new VariousMethods.Student
+            new Student
             {
                Group = "IP-21", Surname = "Лебедева", Name = "Светлана", Dadsname = "Александровна",
                Year = 2001, Gender = 'Ж', Physics = 5, Math = 5, Inf = 5, Grant = 8000
             },
-            new VariousMethods.Student
+            new Student
             {
                Group = "Fiz-22", Surname = "Николаев", Name = "Андрей", Dadsname = "Сергеевич",
                Year = 2008, Gender = 'M', Physics = 3, Math = 2, Inf = 3, Grant = 2500
             },
-            new VariousMethods.Student
+            new Student
             {
                Group = "IP-22", Surname = "Сергеева", Name = "Дарья", Dadsname = "Викторовна",
                Year = 2008, Gender = 'Ж', Physics = 2, Math = 2, Inf = 2, Grant = 2000
             }
          };
          // Запись массива структур в текстовый файл
-         VariousMethods.WriteStructFileTxt(pathStruct, students);
+         MethodsForStruct.WriteStructFileTxt(pathStruct, students);
          // Чтение массива структур из текстового файла
-         VariousMethods.Student[] readStudents = VariousMethods.ReadStructFileTxt(pathStruct, "spisok.txt");
+         Student[] readStudents = MethodsForStruct.ReadStructFileTxt(pathStruct, "spisok.txt");
          // Вывод прочитанных данных
          Console.WriteLine("Прочитанные данные из текстового файла:");
          int i = 0;
          while (i < readStudents.Length)
          {
-            VariousMethods.Student student = readStudents[i];
+            Student student = readStudents[i];
             Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}",
                student.Group, student.Surname, student.Name, student.Dadsname, student.Year,
                student.Gender, student.Physics, student.Math, student.Inf, student.Grant);
@@ -101,15 +115,15 @@ namespace Homework_6._1
 
          Console.WriteLine();
          // Запись массива структур в бинарный файл
-         VariousMethods.WriteStructFileBin(pathWrite, readStudents);
+         MethodsForStruct.WriteStructFileBin(pathWrite, readStudents);
          // Чтение массива структур из бинарного файла
-         VariousMethods.Student[] readCadets = VariousMethods.ReadStructFileBin(pathWrite);
+         Student[] readCadets = MethodsForStruct.ReadStructFileBin(pathWrite);
          // Вывод прочитанных данных
          Console.WriteLine("Прочитанные данные из бинарного файла:");
          int j = 0;
          while (j < readCadets.Length)
          {
-            VariousMethods.Student cadet = readCadets[j];
+            Student cadet = readCadets[j];
             Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}",
                cadet.Group, cadet.Surname, cadet.Name, cadet.Dadsname, cadet.Year,
                cadet.Gender, cadet.Physics, cadet.Math, cadet.Inf, cadet.Grant);
@@ -117,13 +131,13 @@ namespace Homework_6._1
          }
          // Расчет среднего балла всех студентов по всем предметам
          Console.WriteLine();
-         double average = VariousMethods.AverageScore(students);
+         double average = MethodsForStruct.AverageScore(students);
          // Поиск студентов средний балл которых выше, чем общий средний балл
          Console.WriteLine();
-         VariousMethods.AverageHigherScore(pathRead, students, average);
+         MethodsForStruct.AverageHigherScore(pathRead, students, average);
          // Поиск несовершеннолетнего студента с худшим средним баллом
          Console.WriteLine();
-         VariousMethods.MinorStudentWorstAverage(fileInput, students);
+         MethodsForStruct.MinorStudentWorstAverage(fileInput, students);
 
          Console.ReadKey();
       }
